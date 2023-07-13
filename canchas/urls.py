c3c7edfc66fa664d94fd2331e992b63da121cc6f
jwf12 <jwf_12@hotmail.com>
2023-07-13 03:26:30 -0300

    .

--- canchas/urls.py
+++ canchas/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from .views import IndexView, CustomLoginView, SingUpView, CreateReservation2
 
@@ -8,7 +7,7 @@ from .views import IndexView, CustomLoginView, SingUpView, CreateReservation2
 app_name = 'canchas'
 
 urlpatterns = [
-    path('padel/', IndexView.as_view(template_name='index.html'), {'room_name': 'padel'}, name='index'),
+    path('', IndexView.as_view(template_name='index.html'), {'room_name': 'padel'}, name='index'),
     path('tenis/', IndexView.as_view(template_name='tenis.html'), {'room_name': 'tenis'}, name='tenis'),
     path('createreservation/<int:shift_id>/', CreateReservation2.as_view(), name='reservation2'),
     
@@ -19,5 +18,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name = 'login.html'), name='logout'),
 
 
-    
+    path('paypal/', include("paypal.standard.ipn.urls")),
 ]
